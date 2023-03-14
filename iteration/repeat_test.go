71987b456becf90471b8b7cad8a108fca1961bd5
Mirko <mirko.gabski@gmail.com>
2023-03-14 20:59:28 +0100

    run multiple tests

--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -4,12 +4,23 @@ import "testing"
 import "fmt"
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a", 5)
-	expected := "aaaaa"
+	t.Run("Repeat(\"a\", 5)", func(t *testing.T) {
+		repeated := Repeat("a", 5)
+		expected := "aaaaa"
+		assertCorrectMessage(t, repeated, expected)
+	})
 
-	if repeated != expected {
-		t.Errorf("expected %q but got %q", expected, repeated)
-	}
+	t.Run("Repeat(\"x\", 10)", func(t *testing.T) {
+		repeated := Repeat("x", 10)
+		expected := "xxxxxxxxxx"
+		assertCorrectMessage(t, repeated, expected)
+	})
+
+	t.Run("Repeat(\"a\", 0)", func(t *testing.T) {
+		repeated := Repeat("a", 5)
+		expected := ""
+		assertCorrectMessage(t, repeated, expected)
+	})
 }
 
 func BenchmarkRepeat(b *testing.B) {
@@ -22,4 +33,11 @@ func ExampleRepeat() {
 	repeated := Repeat("a", 5)
 	fmt.Println(repeated)
 	// Output: aaaaa
+}
+
+func assertCorrectMessage(t testing.TB, repeated, expected string) {
+	t.Helper()
+	if repeated != expected {
+		t.Errorf("expected %q but got %q", expected, repeated)
+	}
 }
